c2b6b834975c082b2c84047c02439998561f12c9
Darren Shepherd <darren@acorn.io>
2023-03-02 11:32:59 -0700

    Proper response

--- manual/main.go
+++ manual/main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,7 +20,13 @@ type Properties struct {
 }
 
 type Event struct {
-	ResourceProperties Properties `json:"ResourceProperties,omitempty"`
+	RequestType        string     `json:"RequestType"`
+	ResponseURL        string     `json:"ResponseURL"`
+	StackId            string     `json:"StackId"`
+	RequestId          string     `json:"RequestId"`
+	ResourceType       string     `json:"ResourceType"`
+	LogicalResourceId  string     `json:"LogicalResourceId"`
+	ResourceProperties Properties `json:"ResourceProperties"`
 }
 
 type Payload struct {
@@ -29,10 +36,82 @@ type Payload struct {
 	StackID    string `json:"stackID,omitempty"`
 }
 
-func HandleRequest(ctx context.Context, event Event) (string, error) {
+type Response struct {
+	Status             string            `json:"Status"`
+	PhysicalResourceId string            `json:"PhysicalResourceId"`
+	StackId            string            `json:"StackId"`
+	RequestId          string            `json:"RequestId"`
+	LogicalResourceId  string            `json:"LogicalResourceId"`
+	Data               map[string]string `json:"Data"`
+}
+
+type Metadata struct {
+	UID  string `json:"uid,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
+type APIResponse struct {
+	Metadata `json:"metadata,omitempty"`
+}
+
+func logEvent(event Event) {
+	if len(event.ResourceProperties.Token) > 5 {
+		event.ResourceProperties.Token = event.ResourceProperties.Token[:5]
+	}
 	input, _ := json.Marshal(event)
 	fmt.Println("Input ", string(input))
+}
+
+func handleResponse(event Event, r Response) (string, error) {
+	eventResponse, err := json.Marshal(r)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("Uploading to [%s] payload: %s\n", event.ResponseURL, string(eventResponse))
+	req, err := http.NewRequest(http.MethodPut, event.ResponseURL, bytes.NewBuffer(eventResponse))
+	if err != nil {
+		return "", err
+	}
+	req.ContentLength = int64(len(eventResponse))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return string(eventResponse), err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to upload response got [%d]: %s", resp.StatusCode, string(data))
+	}
+
+	return string(eventResponse), err
+}
+
+func HandleRequest(ctx context.Context, event Event) (string, error) {
+	logEvent(event)
+
+	if event.RequestType != "Create" {
+		return handleResponse(event, Response{
+			Status:             "SUCCESS",
+			StackId:            event.StackId,
+			RequestId:          event.RequestId,
+			PhysicalResourceId: "unknown",
+			LogicalResourceId:  event.LogicalResourceId,
+		})
+	}
+
 	payload, err := json.Marshal(map[string]any{
+		"kind":       "AWSAccountRegistrationCallback",
+		"apiVersion": "internal.account.hub.acorn.io/v1",
+		"metadata": map[string]any{
+			"generateName": "create",
+		},
 		"spec": &Payload{
 			Token:      event.ResourceProperties.Token,
 			ExternalID: event.ResourceProperties.ExternalId,
@@ -47,7 +126,33 @@ func HandleRequest(ctx context.Context, event Event) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return string(payload), resp.Body.Close()
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		return "", fmt.Errorf("failed to create resource got [%d]: %s", resp.StatusCode, string(data))
+	}
+
+	var apiResponse APIResponse
+	if err := json.Unmarshal(data, &apiResponse); err != nil {
+		return "", err
+	}
+
+	return handleResponse(event, Response{
+		Status:             "SUCCESS",
+		PhysicalResourceId: apiResponse.UID,
+		StackId:            event.StackId,
+		RequestId:          event.RequestId,
+		LogicalResourceId:  event.LogicalResourceId,
+		Data: map[string]string{
+			"Name": apiResponse.Name,
+			"UID":  apiResponse.UID,
+		},
+	})
 }
 
 func main() {
